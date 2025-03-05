d1bb8524d51617f9181d482413d27e0dcb9f4a34
sureka-123 <surekavincent92@gmail.com>
2025-03-05 19:03:26 +0530

    Add files via upload
    WEEK 1

---
+++ Week1/Day1/Chrome.java
@@ -0,0 +1,20 @@
+package Week1.Day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version=91.0f;
+		String type="Google";
+		boolean isbeta=false;
+		int releaseyear=2008;
+		char shortcutkey='C';
+		System.out.println(version);
+		System.out.println(type);
+		System.out.println(isbeta);
+		System.out.println(releaseyear); 
+		System.out.println(shortcutkey);
+
+		
+	}
+
+}
